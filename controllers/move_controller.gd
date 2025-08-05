dabbb8933ab675eb03625948adecef30eee921b3
ChrisCee2 <chrischung411@gmail.com>
2025-08-05 00:53:11 -0400

    GC4-1: WIP replace lerp, not good for acceleration

--- controllers/move_controller.gd
+++ controllers/move_controller.gd
@@ -4,6 +4,7 @@ class_name MoveController extends Node
 @export var max_acceleration: float = 1.0
 @export var torque: float = 5.0
 @export_range(0, 1) var acceleration_speed: float = 0.1 # How fast it accelerates, using lerp weirdly
+@export_range(0, 1) var deceleration_speed: float = 0.1
 
 var acceleration_multiplier: float = 10000
 var torque_multiplier: float = 10000
@@ -14,26 +15,34 @@ var current_acceleration: float = 0
 func update(direction: Vector2) -> void:
 	if rigid_body == null:
 		return
+		
 	var forward_direction: Vector2 = rigid_body.global_transform.y
-	current_acceleration = lerpf(
-		current_acceleration, 
-		get_desired_acceleration(direction.y), 
-		acceleration_speed
-	)
+		
+	# Apply facing direction force
+	current_acceleration = get_current_acceleration(direction.y)
 	rigid_body.apply_central_force(
 		forward_direction * current_acceleration * acceleration_multiplier * rigid_body.mass
 	)
-	if rigid_body.linear_velocity.length() >= 200 and direction.x != 0:
+	
+	# Apply torque
+	if rigid_body.linear_velocity.length() >= 100 and direction.x != 0:
 		# Consider scaling torque to current velocity instead of this value
-		if direction.y != 0:
-			torque_direction = direction.x if direction.y < 0 else -direction.x
+		torque_direction = 1 if rigid_body.linear_velocity.dot(forward_direction) < 0 else -1
 		rigid_body.apply_torque(
-			torque_direction * torque * torque_multiplier * rigid_body.mass
+			torque_direction * direction.x * torque * torque_multiplier * rigid_body.mass
 		)
 
 
-func get_desired_acceleration(direction_y: float) -> float:
-	if direction_y == 0:
-		return 0.0
-	return max_acceleration if direction_y > 0 else -max_acceleration
-		
+func get_current_acceleration(direction_y: float) -> float:
+	var desired_acceleration: float = 0.0
+	if direction_y != 0:
+		desired_acceleration = max_acceleration if direction_y > 0 else -max_acceleration
+	
+	var lerp_speed: float = deceleration_speed if desired_acceleration == 0.0 else acceleration_speed
+	
+	return lerpf(
+		current_acceleration, 
+		desired_acceleration, 
+		lerp_speed
+	)
+	
