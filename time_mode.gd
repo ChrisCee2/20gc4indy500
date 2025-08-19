5e226523adb65211ab11e4e2f731556ef4d1d2c6
ChrisCee2 <chrischung411@gmail.com>
2025-08-19 01:06:39 -0400

    GC4-4: Add start and track time labels

--- time_mode.gd
+++ time_mode.gd
@@ -4,12 +4,20 @@ class_name TimeMode extends Node
 @onready var start_timer: Timer = $Timer
 
 
-@export var laps: int = 2
+@export_group("Required Nodes")
 @export var players: Node
+@export var start_timer_label: Label
+@export var track_time_label: Label
+@export_group("Settings")
+@export var laps: int = 2
 
 
 var start_time: int = 0
 var time_to_start: float = 3
+var is_started: bool = false
+var is_starting: bool = false
+var is_finished: bool = false
+var finish_time: int = 0
 
 
 func _ready() -> void:
@@ -20,28 +28,54 @@ func _ready() -> void:
 		if object is Player:
 			object.disabled = true
 			object.lapped.connect(check_win_condition.bind(object))
+	
+	start_timer_label.hide()
+	
+	init()
+
+
+func _process(delta: float) -> void:
+	if is_starting:
+		start_timer_label.text = str(ceili(start_timer.time_left))
+	if is_started:
+		if is_finished:
+			track_time_label.text = get_formatted_time(finish_time)
+		else:
+			track_time_label.text = get_formatted_time(get_current_time())
 
 
 func start() -> void:
-	# Set up timer ui with
-	# Also hide the start timer ui
+	is_starting = false
+	start_timer_label.hide()
+	track_time_label.show()
 	start_time = Time.get_ticks_msec()
+	is_started = true
+	is_finished = false
 	for object in players.get_children():
 		if object is Player:
 			object.disabled = false
 
 
 func init() -> void:
+	is_starting = true
+	start_timer_label.show()
 	start_timer.start(time_to_start)
-	# Have UI connected to this timer?
 
 
 func get_current_time() -> int:
-	# use this for Timer ui
 	return Time.get_ticks_msec() - start_time
 
 
 func check_win_condition(player: Player) -> void:
 	if player.laps_completed >= laps:
-		var finish_time: int = get_current_time()
-		# Stop timer UI and show press space bar to go back to main menu
+		finish_time = get_current_time()
+		is_finished = true
+		# Show press space bar to go back to main menu
+
+
+func get_formatted_time(ms: int) -> String:
+	var minutes: int = (ms / 60000)
+	var seconds: int = ((ms % 60000) / 1000)
+	var milliseconds: int = int(ms % 1000)
+
+	return "%d:%02d.%03d" % [minutes, seconds, milliseconds]
