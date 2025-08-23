1f74b48d8c5cc08867f171906ab0809de1d96f7c
ChrisCee2 <chrischung411@gmail.com>
2025-08-23 00:55:34 -0400

    GC4-10: WIP fixed spawn, need to fix map collisions

--- tracks/track.gd
+++ tracks/track.gd
@@ -3,6 +3,7 @@ class_name Track extends Node
 @export var spawn: Node2D
 @export var spawn_offset: float = 64 # From top downwards
 @export var spawn_angle: float = 90 # Degrees
+@export var player_spawn_angle: float = -90 # Degrees
 
 
 func spawn_players(players: Node) -> void:
@@ -11,5 +12,5 @@ func spawn_players(players: Node) -> void:
 		var offset_direction: Vector2 = Vector2.RIGHT.rotated(deg_to_rad(spawn_angle))
 		for player in players.get_children():
 			player.global_position = spawn.global_position + (current_spawn_offset * offset_direction)
-			player.global_rotation = -deg_to_rad(spawn_angle)
+			player.global_rotation = deg_to_rad(player_spawn_angle)
 			current_spawn_offset += spawn_offset
