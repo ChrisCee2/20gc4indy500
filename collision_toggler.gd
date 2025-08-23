e58feb90db7b2a66736158be97703e856b571a19
ChrisCee2 <chrischung411@gmail.com>
2025-08-23 01:24:15 -0400

    GC4-10: Add z layer illusion of depth

--- collision_toggler.gd
+++ collision_toggler.gd
@@ -2,6 +2,8 @@ class_name CollisionToggler extends Area2D
 
 
 @export var mask: int = 1
+@export var update_z_layer: bool = false
+@export var z_layer: int = 0
 
 
 func _ready() -> void:
@@ -11,3 +13,5 @@ func _ready() -> void:
 func _on_enter(body: Node2D) -> void:
 	if body is Player:
 		body.collision_mask = mask
+		if update_z_layer:
+			body.z_index = z_layer
